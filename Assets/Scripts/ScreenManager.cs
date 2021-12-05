9850eea19af44f564c84c32502f3b3dd6468b9a8
DukeTresnor <armstrongbradford@gmail.com>
2021-12-04 22:12:46 -0800

    undo of previous edit

--- Assets/Scripts/ScreenManager.cs
+++ Assets/Scripts/ScreenManager.cs
@@ -40,7 +40,7 @@ public class ScreenManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
